5f45c623cd3dea256907ff31ea95ab43d2a1af7c
Alessandro Dias <alebatista90@terra.com.br>
2014-03-18 20:31:53 -0300

    dependent: :destroy

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,4 +1,4 @@
 class Repository < ActiveRecord::Base
   validates_uniqueness_of :url, message: "This repository already exists"
-  has_many :class_details
+  has_many :class_details, dependent: :destroy
 end

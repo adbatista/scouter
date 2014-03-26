19995752c9c4263ad9e068822bdff65e62d00bdf
Alessandro Dias <alebatista90@terra.com.br>
2014-03-26 01:47:58 -0300

    #class_detail now return methods

--- lib/processor.rb
+++ lib/processor.rb
@@ -23,7 +23,19 @@ class Processor
     @metric_processor.result.result_hash[:flog][:method_containers].map do |class_details|
       {
         class_name: class_details[:name],
-        score:      class_details[:total_score]
+        score:      class_details[:total_score],
+        method_details: method_details(class_details[:methods])
+      }
+    end
+  end
+
+  private
+  def method_details methods
+    puts methods
+    methods.map do |method_full_name, details|
+      {
+        name: method_full_name,
+        score: details[:score]
       }
     end
   end

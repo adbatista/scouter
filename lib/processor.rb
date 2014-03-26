d37aaaa560d72e1fa169e66d4a071ba5150b28f7
Alessandro Dias <alebatista90@terra.com.br>
2014-03-25 23:22:43 -0300

    clean up processor

--- lib/processor.rb
+++ lib/processor.rb
@@ -1,13 +1,7 @@
-require 'forwardable'
 require 'metric_fu'
+require 'metric_fu/run'
 
 class Processor
-  extend Forwardable
-
-  def_delegator :@complexity_processor, :flog, :analyze_complexity
-
-  attr_reader :repository
-
   def initialize metric=MetricFu, repo_handler=RepositoryHandler.new
     @metric_processor   = metric
     @repository_handler = repo_handler
@@ -15,18 +9,21 @@ class Processor
 
   def process repository
     path = @repository_handler.create_repository repository
+    options = {}
+
     Dir.chdir path do
-      @metric_processor.run_only %w{ flog }
+      @metric_processor.run_only %w{ flog }, options
     end
+
   ensure
     @repository_handler.destroy_repository path
   end
 
   def class_details
-    @metric_processor.result[:flog][:method_containers].map do |method_container|
+    @metric_processor.result.result_hash[:flog][:method_containers].map do |class_details|
       {
-        class_name: method_container[:name],
-        score:      method_container[:total_score]
+        class_name: class_details[:name],
+        score:      class_details[:total_score]
       }
     end
   end
--- spec/lib/processor_spec.rb
+++ spec/lib/processor_spec.rb
@@ -8,7 +8,7 @@ describe Processor do
   describe "#class_details" do
     it "return complexity metrics" do
       metric_result = { flog: { :method_containers=>[ { name: "Test", total_score: 10.7 } ] }}
-      allow(metric).to receive(:result).and_return(metric_result)
+      metric.stub_chain(:result, :result_hash).and_return(metric_result)
 
       expect(subject.class_details).to include({ class_name: 'Test', score:  10.7 })
     end
@@ -22,7 +22,7 @@ describe Processor do
     end
 
     it "process repository" do
-      expect(metric).to receive(:run_only).with(%w{ flog })
+      expect(metric).to receive(:run_only).with(%w{ flog },{})
 
       subject.process repo
     end
